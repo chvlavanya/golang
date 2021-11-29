9f45b8a4a9045f8aaf7b3a9016b8b1972d598cc7
chvlavanya <chvlavanya@users.noreply.github.com>
2021-11-29 17:53:32 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+	var name = "Lavanya"
+	var city = "Allentown"
+	var state = "PA"
+	fmt.Println("Name:", name)
+	fmt.Println("City:", city)
+	fmt.Println("State:", state)
+}
+
+
