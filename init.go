b40ce61345eca93e46644b958ca499e20ecc633f
chvlavanya <chvlavanya@users.noreply.github.com>
2021-11-30 21:10:44 +0000

    condition-init-loops-switch

---
+++ init.go
@@ -0,0 +1,13 @@
+ /*  init function              */
+
+package main
+
+import "fmt"
+
+func init() {
+  fmt.Println("Think of the init function like a prelude to main.")
+}
+
+func main() {
+  fmt.Println("This would run 'after' the init.")
+}
