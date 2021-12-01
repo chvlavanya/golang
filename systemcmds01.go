369ac72d7bf5a506bf4d77fc932c03b2f581e102
chvlavanya <chvlavanya@users.noreply.github.com>
2021-12-01 21:29:37 +0000

    issuing system commands with Go

---
+++ systemcmds01.go
@@ -0,0 +1,23 @@
+/* Alta3 Research | RZFeeser
+   Executing system commands  */
+
+package main
+
+import (
+    "log"
+    "os/exec"
+    "fmt"
+)
+
+func main() {
+
+    // prepares a "cmd" struct
+    cmd := exec.Command("ls")
+
+    err := cmd.Run()
+   
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
